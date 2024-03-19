cf9de674f5d7a660723e98b8d9bf7f54f7d6966f
romb52 <rom.bom1980@gmail.com>
2024-03-19 20:49:16 +0200

    fix sound path

--- src/share/audioUtils.js
+++ src/share/audioUtils.js
@@ -1,5 +1,6 @@
 export const playSound = (soundFile) => {
     //const audio = new Audio(`/sound/${soundFile}`); 
+    //console.error(audio);
     const audio = new Audio(`/15_puzzle_game_A-/sound/${soundFile}`); //за умови розгортання сайту на https://romb52.github.io/15_puzzle_game_A-/
     audio.play().catch((error) => {      
       console.error('Failed to play sound:', error);
