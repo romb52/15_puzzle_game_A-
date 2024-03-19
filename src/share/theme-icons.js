cd534d0f749e3618647a125176d30d95c278e82b
romb52 <rom.bom1980@gmail.com>
2024-03-19 18:06:33 +0200

    Updates

--- src/share/theme-icons.js
+++
@@ -1,9 +0,0 @@
-import { FaMoon, FaSun, FaSnowflake   } from "react-icons/fa";
-import { GiPalmTree } from "react-icons/gi";
-
-export const themeIcons = {
-    dark: <FaMoon />,
-    light: <FaSun />,
-    winter: <FaSnowflake />,
-    summer: <GiPalmTree />
-}
